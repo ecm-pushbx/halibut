6c393e0b85658f39b2324346685f1bcdfa1012c1
Simon Tatham <anakin@pobox.com>
2014-09-30 20:09:31 +0000

    Provide a static version.h for development builds.
    I forgot to add this in last week's versioning revamp, so that bob
    builds work (constructing a version.h from the build script) but dev
    builds straight from source control fail for lack of version.h.
    
    [originally from svn r10277]

---
+++ version.h
@@ -0,0 +1,11 @@
+/*
+ * This header file provides the versioning-related #defines for a
+ * particular build of Halibut.
+ *
+ * When my automated build system does a full build, Buildscr
+ * completely overwrites this file with information derived from the
+ * circumstances and type of that build. The information _here_ is
+ * default stuff used for local development runs of 'make'.
+ */
+
+#define VERSION "unidentified version"
